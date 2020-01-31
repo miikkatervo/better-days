db88cee457f4fed3aa9a26f61334a3346de06d7c
Miikka Tervo <miikka.tervo@gmail.com>
2020-01-31 18:08:35 +0200

    survey shows data

--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./survey.component.css']
 })
 export class SurveyComponent implements OnInit {
-
+  name:string = "Miikka";
   constructor() { }
 
   ngOnInit() {
